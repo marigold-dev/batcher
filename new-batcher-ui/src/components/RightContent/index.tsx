af4a00cf8406abd59c17a15f74a062e38fc1d813
kienle <kienle371999@gmail.com>
2022-10-19 11:08:51 +0700

    Responsive for menu buttons

--- new-batcher-ui/src/components/RightContent/index.tsx
+++ new-batcher-ui/src/components/RightContent/index.tsx
@@ -1,9 +1,29 @@
-import { Space, Button } from 'antd';
+import { Space, Button, Menu, Dropdown, Typography } from 'antd';
 import React from 'react';
 import { useModel, FormattedMessage } from 'umi';
 import styles from './index.less';
+import { MenuOutlined } from '@ant-design/icons';
 
 export type SiderTheme = 'light' | 'dark';
+
+const menu = (
+  <Menu
+    items={[
+      {
+        key: '1',
+        label: <Typography>Connect Wallet</Typography>,
+      },
+      {
+        key: '2',
+        label: <Typography>Order books</Typography>,
+      },
+      {
+        key: '3',
+        label: <Typography>Redeem holdings</Typography>,
+      },
+    ]}
+  />
+);
 
 const GlobalHeaderRight: React.FC = () => {
   const { initialState } = useModel('@@initialState');
@@ -20,9 +40,12 @@ const GlobalHeaderRight: React.FC = () => {
   }
   return (
     <Space className={className}>
-      <Button type="primary" danger>
+      <Button className="batcher-connect-wallet" type="primary" danger>
         <FormattedMessage id="pages.searchTable.batchDeletion" defaultMessage="Connect Wallet" />
       </Button>
+      <Dropdown overlay={menu} placement="bottomLeft">
+        <MenuOutlined className="batcher-menu" />
+      </Dropdown>
     </Space>
   );
 };
