29d158b758513034c2f94627062bc42731b75753
kienle <kienle371999@gmail.com>
2022-10-19 09:38:09 +0700

    First version for responsive design

--- new-batcher-ui/src/pages/Welcome.tsx
+++ new-batcher-ui/src/pages/Welcome.tsx
@@ -7,22 +7,22 @@ const Welcome: React.FC = () => {
     <div>
       <Row className="batcher-header">
         <Col lg={3} />
-        <Col className="br-t br-l" xs={24} lg={6}>
+        <Col className="batcher-time" xs={24} lg={6}>
           <Space direction="vertical">
             <Typography className="batcher-title p-16">Batcher Time Remaining</Typography>
             <Typography className="batcher-title p-13">Open Batch</Typography>
           </Space>
         </Col>
-        <Col className="br-t br-l" xs={24} lg={6}>
-          <Col className="br-b grid-padding" span={24}>
+        <Col className="batcher-balance" xs={24} lg={6}>
+          <Col className="batcher-balance-title" span={24}>
             <Typography className="batcher-title p-16">Balance</Typography>
           </Col>
-          <Col className="grid-padding" span={24}>
-            <Typography className="batcher-title p-13">Address No wallet connected</Typography>
+          <Col className="batcher-balance-amount" span={24}>
+            <Typography className="batcher-title p-16">Address No wallet connected</Typography>
           </Col>
         </Col>
-        <Col className="br-t br-l br-r" xs={24} lg={6}>
-          <Space direction="vertical">
+        <Col className="batcher-oracle" xs={24} lg={6}>
+          <Space>
             <Typography className="batcher-title p-16">Oracle Price</Typography>
             <Typography className="batcher-title p-13">0 tzBTC/USDT</Typography>
           </Space>
@@ -39,14 +39,7 @@ const Welcome: React.FC = () => {
                 <Space className="batcher-price" direction="vertical">
                   <Space size="large">
                     <Typography className="batcher-title p-16">From tzBTC</Typography>
-                    <Input
-                      suffix={
-                        <div className="grid-display">
-                          <UpOutlined />
-                          <DownOutlined />
-                        </div>
-                      }
-                    />
+                    <Input className="batcher-token" placeholder="Amount" />
                   </Space>
                   <Typography className="batcher-title p-13">
                     Select the price you want to sell
