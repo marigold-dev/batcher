e3cb9123effd51ec16884f96f238620d63e323cc
kienle <kienle371999@gmail.com>
2022-10-25 09:53:37 +0700

    Correct the web-socket errors in computing token balances

--- batcher-ui/src/extra_utils/utils.ts
+++ batcher-ui/src/extra_utils/utils.ts
@@ -19,11 +19,10 @@ export const scaleAmountUp = (amount: number, decimals: number) => {
 export const getSocketTokenAmount = (
   balances: Array<any>,
   userAddress: string,
-  standardBalance: any,
+  standardBalance: types.token_balance,
 ) => {
   const item = balances.find((item) => item.account.address === userAddress);
-  console.log(666, item, standardBalance);
-  return item ? parseInt(item.balance) / 10 ** standardBalance.decimal : 0;
+  return item ? parseInt(item.balance) / 10 ** standardBalance.token.decimals : 0;
 };
 
 export const getErrorMess = (error: unknown) => {
