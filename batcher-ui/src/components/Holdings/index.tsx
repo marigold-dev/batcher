f2c842e735e12ff84dbe3e351c7387b489ce0a69
Kiên Lê <37668434+kienle371999@users.noreply.github.com>
2022-10-28 18:35:55 +0700

    Change the pop-up and update holding layout (#181)
    * Change the pop-up
    
    * Update the holding layout
    
    * Remove title in holdings

--- batcher-ui/src/components/Holdings/index.tsx
+++ batcher-ui/src/components/Holdings/index.tsx
@@ -1,146 +1,136 @@
 import React, { useEffect, useState } from 'react';
-import { SwapOutlined } from '@ant-design/icons';
-import { Input, Button, Space, Typography, Col, Row, message } from 'antd';
+import { Button, Space, Typography, Col, message } from 'antd';
 import { useModel } from 'umi';
 import '@/components/Exchange/index.less';
+import '@/components/Holdings/index.less';
 import '@/global.less';
-import { HoldingsProps, batch, token_amount } from '@/extra_utils/types';
+import { HoldingsProps, token_amount } from '@/extra_utils/types';
 import { scaleAmountDown } from '@/extra_utils/utils';
-import { JSONPath } from "jsonpath-plus";
-import toast from "react-hot-toast";
-import MarigoldLogoSvg from '../../../img/marigold-logo.svg';
+import { JSONPath } from 'jsonpath-plus';
 
-const { Text } = Typography;
-
-const Holdings: React.FC<HoldingsProps> = ({tezos, bigMapsByIdUri,userAddress, contractAddress, previousTreasuries, buyToken, sellToken }: HoldingsProps) => {
-  const [inversion, setInversion] = useState(true);
+const Holdings: React.FC<HoldingsProps> = ({
+  tezos,
+  bigMapsByIdUri,
+  userAddress,
+  contractAddress,
+  previousTreasuries,
+  buyToken,
+  sellToken,
+}: HoldingsProps) => {
   const { initialState } = useModel('@@initialState');
 
   const [buyTokenHoldings, setBuyTokenHoldings] = useState<number>(0);
   const [sellTokenHoldings, setSellTokenHoldings] = useState<number>(0);
 
   const update_holdings = async () => {
-    console.log('Holdings-previousBatches',previousTreasuries);
+    console.log('Holdings-previousBatches', previousTreasuries);
     let buy_holdings = 0;
     let sell_holdings = 0;
 
     setBuyTokenHoldings(buy_holdings);
     setSellTokenHoldings(sell_holdings);
 
-    for(var i =0; i < previousTreasuries?.length; i++) {
-     try{
-       let bm_uri = bigMapsByIdUri + previousTreasuries.at(i) + "/keys/" + userAddress;
-       console.log('Holdings-bm-uri',bm_uri);
-       const data = await fetch(bm_uri, {
-          method: "GET"
-       });
-       const jsonData = await data.json();
+    for (var i = 0; i < previousTreasuries?.length; i++) {
+      try {
+        let bm_uri = bigMapsByIdUri + previousTreasuries.at(i) + '/keys/' + userAddress;
+        console.log('Holdings-bm-uri', bm_uri);
+        const data = await fetch(bm_uri, {
+          method: 'GET',
+        });
+        const jsonData = await data.json();
 
-       if(!jsonData.active) {
+        if (!jsonData.active) {
           break;
-       }
-
-       const amounts = JSONPath({ path: "$.value.*.token_amount", json:jsonData});
-       const tokenAmounts = amounts as Array<token_amount>;
-       console.log('Holdings-token-amounts',tokenAmounts);
-       buy_holdings += tokenAmounts.reduce((prev, token_amount) => {
-        if (token_amount.token.name === buyToken.name){
-          prev += scaleAmountDown(token_amount.amount, buyToken.decimals);
         }
-        return prev;
-       },0);
 
-       sell_holdings += tokenAmounts.reduce((prev, token_amount) => {
-        if (token_amount.token.name === sellToken.name){
-          prev += scaleAmountDown(token_amount.amount, sellToken.decimals);
-        }
-        return prev;
-       },0);
+        const amounts = JSONPath({ path: '$.value.*.token_amount', json: jsonData });
+        const tokenAmounts = amounts as Array<token_amount>;
+        console.log('Holdings-token-amounts', tokenAmounts);
+        buy_holdings += tokenAmounts.reduce((prev, token_amount) => {
+          if (token_amount.token.name === buyToken.name) {
+            prev += scaleAmountDown(token_amount.amount, buyToken.decimals);
+          }
+          return prev;
+        }, 0);
 
-       setBuyTokenHoldings(buy_holdings);
-       setSellTokenHoldings(sell_holdings);
+        sell_holdings += tokenAmounts.reduce((prev, token_amount) => {
+          if (token_amount.token.name === sellToken.name) {
+            prev += scaleAmountDown(token_amount.amount, sellToken.decimals);
+          }
+          return prev;
+        }, 0);
 
-      } catch (error : any) {
+        setBuyTokenHoldings(buy_holdings);
+        setSellTokenHoldings(sell_holdings);
+      } catch (error: any) {
         console.log(error);
       }
     }
   };
 
-  const redeemHoldings = async () : Promise<void> => {
-    console.log("redeeming");
-    message.loading("Attempting to redeem holdings...");
-     try{
-        const contractWallet = await tezos.wallet.at(contractAddress);
-        const buyTokenWalletContract = await tezos.wallet.at(buyToken.address);
-        const sellTokenWalletContract = await tezos.wallet.at(sellToken.address);
+  const redeemHoldings = async (): Promise<void> => {
+    console.log('redeeming');
+    message.loading('Attempting to redeem holdings...');
+    try {
+      const contractWallet = await tezos.wallet.at(contractAddress);
+      const buyTokenWalletContract = await tezos.wallet.at(buyToken.address);
+      const sellTokenWalletContract = await tezos.wallet.at(sellToken.address);
 
-        const operator_params = [
-         {
-            remove_operator : {
-              owner: userAddress,
-              operator: contractAddress,
-              token_id: 0
-            }
-         }
-        ];
+      const operator_params = [
+        {
+          remove_operator: {
+            owner: userAddress,
+            operator: contractAddress,
+            token_id: 0,
+          },
+        },
+      ];
 
-        const redeem_op = await tezos.wallet.batch()
-           .withContractCall(contractWallet.methodsObject.redeem())
-           .withContractCall(buyTokenWalletContract.methods.update_operators(operator_params))
-           .withContractCall(sellTokenWalletContract.methods.update_operators(operator_params))
-           .send();
+      const redeem_op = await tezos.wallet
+        .batch()
+        .withContractCall(contractWallet.methodsObject.redeem())
+        .withContractCall(buyTokenWalletContract.methods.update_operators(operator_params))
+        .withContractCall(sellTokenWalletContract.methods.update_operators(operator_params))
+        .send();
 
-        const confirm = await redeem_op.confirmation();
-        if(!confirm.completed){
-           message.error("Failed to redeem holdings");
-           console.error("Failed to redeem holdings" + confirm);
-        } else {
-           message.success("Successfully redeemed holdings");
-        }
-     } catch (error:any){
-           message.error("Unable to redeem holdings : " + error.message);
-           console.error("Unable to redeem holdings" + error);
-     }
+      const confirm = await redeem_op.confirmation();
+      if (!confirm.completed) {
+        message.error('Failed to redeem holdings');
+        console.error('Failed to redeem holdings' + confirm);
+      } else {
+        message.success('Successfully redeemed holdings');
+      }
+    } catch (error: any) {
+      message.error('Unable to redeem holdings : ' + error.message);
+      console.error('Unable to redeem holdings' + error);
+    }
   };
 
   useEffect(() => {
-   (async () => update_holdings())();
-  }, [initialState,userAddress]);
-
+    (async () => update_holdings())();
+  }, [initialState, userAddress]);
 
   return (
-              <Col className="base-content br-t br-b br-l br-r">
-                <Space className="batcher-price" direction="vertical">
-                  <Row>
-                    <Col className="mr-c" span={5}>
-                      <Typography className="batcher-title p-16">
-                       Holdings
-                      </Typography>
-                    </Col>
-                  </Row>
-                      <Row className="text-center">
-                    <Col className="batcher-title br-t br-b" span={8} offset={8}>
-                        <Space  direction="vertical">
-                       <Typography className="p-12" autosize>{buyTokenHoldings} {buyToken.name}</Typography>
-                       </Space>
-                       </Col>
-                       </Row>
-                      <Row className="text-center">
-                    <Col className="batcher-title br-t br-b" span={8} offset={8}>
-                        <Space  direction="vertical">
-                       <Typography className="p-12">{sellTokenHoldings} {sellToken.name}</Typography>
-                       </Space>
-                       </Col>
-                      </Row>
-                      <Row className="text-center">
-                    <Col span={8} offset={8}>
-                      <Button className="mtb-25" type="primary" onClick={redeemHoldings} danger>
-                         Redeem
-                      </Button>
-                       </Col>
-                    </Row>
-                    </Space>
-              </Col>
+    <Col className="base-content br-t br-b br-l br-r">
+      <Space className="batcher-price" direction="vertical">
+        <Typography className="batcher-title p-16">Holdings</Typography>
+        <Col className="batcher-holding-content br-t br-b br-l br-r pd-25 tx-align" span={24}>
+          <Space direction="vertical">
+            <Typography>
+              {buyTokenHoldings} {buyToken.name}
+            </Typography>
+            <Typography>
+              {sellTokenHoldings} {sellToken.name}
+            </Typography>
+          </Space>
+        </Col>
+        <Col className="batcher-redeem-btn">
+          <Button className="btn-content mtb-25" type="primary" onClick={redeemHoldings} danger>
+            Redeem
+          </Button>
+        </Col>
+      </Space>
+    </Col>
   );
 };
 
--- batcher-ui/src/components/OrderBook/index.tsx
+++ batcher-ui/src/components/OrderBook/index.tsx
@@ -1,210 +1,228 @@
 import React, { useEffect, useState } from 'react';
-import { SwapOutlined } from '@ant-design/icons';
-import { Input, Button, Space, Typography, Col, Row, InputNumber, Table } from 'antd';
+import { Button, Space, Typography, Col, Row, Table } from 'antd';
 import { useModel } from 'umi';
 import '@/components/Exchange/index.less';
+import '@/components/OrderBook/index.less';
 import '@/global.less';
-import { OrderBookProps, list_of_orders, aggregate_orders, swap_order, Tolerance } from '@/extra_utils/types';
+import {
+  OrderBookProps,
+  list_of_orders,
+  aggregate_orders,
+  swap_order,
+  Tolerance,
+} from '@/extra_utils/types';
 import { orders_exist_in_order_book, scaleAmountDown } from '@/extra_utils/utils';
-import {ColumnCount} from 'antd/lib/list';
-import { ColumnsType } from "antd/es/table";
+import { ColumnsType } from 'antd/es/table';
 
 const { Text } = Typography;
 
-const OrderBook: React.FC<OrderBookProps> = ({ orderBook, buyToken, sellToken }: OrderBookProps) => {
-
+const OrderBook: React.FC<OrderBookProps> = ({
+  orderBook,
+  buyToken,
+  sellToken,
+}: OrderBookProps) => {
   interface AggregateOrder {
     buyside: number;
-    sellside:number;
+    sellside: number;
   }
   interface OrderListItem {
-    order_number:number;
+    order_number: number;
     ordertype: string;
     price: string;
     value: number;
   }
   const { initialState } = useModel('@@initialState');
-  const { wallet, userAddress } = initialState;
   const [aggregateOrdersForTable, setAggregateOrdersForTable] = useState<Array<AggregateOrder>>([]);
   const [aggregateOrdersLoading, setAggregateOrdersLoading] = useState<boolean>(true);
   const [orderListForTable, setOrderListForTable] = useState<Array<OrderListItem>>([]);
-  const [orderListForTableExpanded, setOrderListForTableExpanded] = useState<Array<OrderListItem>>([]);
+  const [orderListForTableExpanded, setOrderListForTableExpanded] = useState<Array<OrderListItem>>(
+    [],
+  );
   const [orderListForTableLoading, setOrderListForTableLoading] = useState<boolean>(true);
   const [expandedView, setExpandedView] = useState<boolean>(false);
-  const leftAggName = buyToken.name + " -> " + sellToken.name;
-  const rightAggName = sellToken.name + " -> " + buyToken.name;
+  const leftAggName = buyToken.name + ' -> ' + sellToken.name;
+  const rightAggName = sellToken.name + ' -> ' + buyToken.name;
 
+  const aggregateAmounts = (orders: Array<swap_order>) => {
+    console.log('OrderBook-aggregate-amounts-orders', orders);
+    return orders.reduce((prev, order) => {
+      prev += Number(order.swap.from.amount);
+      return prev;
+    }, 0);
+  };
 
-   const aggregateAmounts = (orders: Array<swap_order>) => {
-      console.log('OrderBook-aggregate-amounts-orders',orders);
-      return  orders.reduce((prev, order) =>{
-           prev += Number(order.swap.from.amount);
-           return prev;
-       },0)
-   };
+  const to_string_tolerance = (tolerance: Tolerance) => {
+    console.log('OrderBook-tolerance', tolerance);
+    if (tolerance.eXACT != undefined) {
+      return 'Oracle Price';
+    }
 
-   const to_string_tolerance = (tolerance:Tolerance) => {
-      console.log('OrderBook-tolerance', tolerance);
-      if (tolerance.eXACT != undefined){
-         return "Oracle Price";
-      }
+    if (tolerance.mINUS != undefined) {
+      return 'W. price/ B.fill';
+    }
 
-      if (tolerance.mINUS != undefined){
-         return "W. price/ B.fill";
-      }
+    if (tolerance.pLUS != undefined) {
+      return 'B. price/ W.fill';
+    }
 
-      if (tolerance.pLUS != undefined){
-         return "B. price/ W.fill";
-      }
+    return 'Oracle Price';
+  };
 
-      return "Oracle Price";
-   };
-
-   const to_order_for_list = (isBuySide:boolean, order: swap_order) => {
-      return {
-        ordertype: isBuySide ? leftAggName : rightAggName,
-        price: to_string_tolerance(order.tolerance) ,
-        value: isBuySide ? scaleAmountDown(order.swap.from.amount, buyToken.decimals) : scaleAmountDown(order.swap.from.amount,sellToken.decimals),
-      }
-   };
-
-   const update_list_of_orders = async () => {
-     let lofo : Array<list_of_orders> = [];
-
-
-     if(orders_exist_in_order_book(orderBook)){
-       orderBook.bids.map((o) => to_order_for_list(true,o)).forEach(o => lofo.push(o));
-       orderBook.asks.map((o) => to_order_for_list(false,o)).forEach(o => lofo.push(o));
-     }
-
-     let ordernum=0;
-
-     const list_orders_for_table  = lofo.map((o:list_of_orders) => {
-        const modified : OrderListItem =  {
-            order_number: ordernum+1,
-            ordertype: o.ordertype,
-            price: o.price,
-            value: o.value,
-         };
-         return modified
-        });
-    setOrderListForTableExpanded(list_orders_for_table);
-    setOrderListForTable(list_orders_for_table.slice(0,3));
-   };
-
-    const update_aggregate_orders = async () => {
-     let buySideAmount = 0;
-     let sellSideAmount = 0;
-
-     if(orders_exist_in_order_book(orderBook)){
-       buySideAmount = scaleAmountDown(aggregateAmounts(orderBook.bids), buyToken.decimals);
-       sellSideAmount =scaleAmountDown(aggregateAmounts(orderBook.asks), sellToken.decimals);
-
-
-
-      let agg_ord : aggregate_orders =
-        {
-          buyside: buySideAmount,
-          sellside: sellSideAmount
-        };
-
-
-        const agg_orders_for_table  = [agg_ord].map((ao:aggregate_orders) => {
-          const modified : AggregateOrder =  {
-            buyside: ao.buyside,
-            sellside: ao.sellside,
-         };
-         return modified
-        });
-        setAggregateOrdersForTable(agg_orders_for_table);
-     }
+  const to_order_for_list = (isBuySide: boolean, order: swap_order) => {
+    return {
+      ordertype: isBuySide ? leftAggName : rightAggName,
+      price: to_string_tolerance(order.tolerance),
+      value: isBuySide
+        ? scaleAmountDown(order.swap.from.amount, buyToken.decimals)
+        : scaleAmountDown(order.swap.from.amount, sellToken.decimals),
     };
+  };
+
+  const update_list_of_orders = async () => {
+    let lofo: Array<list_of_orders> = [];
+
+    if (orders_exist_in_order_book(orderBook)) {
+      orderBook.bids.map((o) => to_order_for_list(true, o)).forEach((o) => lofo.push(o));
+      orderBook.asks.map((o) => to_order_for_list(false, o)).forEach((o) => lofo.push(o));
+    }
+
+    let ordernum = 0;
+
+    const list_orders_for_table = lofo.map((o: list_of_orders) => {
+      const modified: OrderListItem = {
+        order_number: ordernum + 1,
+        ordertype: o.ordertype,
+        price: o.price,
+        value: o.value,
+      };
+      return modified;
+    });
+    setOrderListForTableExpanded(list_orders_for_table);
+    setOrderListForTable(list_orders_for_table.slice(0, 3));
+  };
+
+  const update_aggregate_orders = async () => {
+    let buySideAmount = 0;
+    let sellSideAmount = 0;
+
+    if (orders_exist_in_order_book(orderBook)) {
+      buySideAmount = scaleAmountDown(aggregateAmounts(orderBook.bids), buyToken.decimals);
+      sellSideAmount = scaleAmountDown(aggregateAmounts(orderBook.asks), sellToken.decimals);
+
+      let agg_ord: aggregate_orders = {
+        buyside: buySideAmount,
+        sellside: sellSideAmount,
+      };
+
+      const agg_orders_for_table = [agg_ord].map((ao: aggregate_orders) => {
+        const modified: AggregateOrder = {
+          buyside: ao.buyside,
+          sellside: ao.sellside,
+        };
+        return modified;
+      });
+      setAggregateOrdersForTable(agg_orders_for_table);
+    }
+  };
 
   const update_orders = async () => {
-     if(orders_exist_in_order_book(orderBook)){
-        update_list_of_orders();
-        update_aggregate_orders();
-        setAggregateOrdersLoading(false);
-        setOrderListForTableLoading(false)
-     }
+    if (orders_exist_in_order_book(orderBook)) {
+      update_list_of_orders();
+      update_aggregate_orders();
+      setAggregateOrdersLoading(false);
+      setOrderListForTableLoading(false);
+    }
   };
 
   useEffect(() => {
     update_orders();
-  }, [ orderBook]);
+  }, [orderBook]);
 
-  const setView= () => {
-     setExpandedView(!expandedView);
+  const setView = () => {
+    setExpandedView(!expandedView);
   };
 
- const aggregateOrdersColumns: ColumnsType<AggregateOrder> = [
+  const aggregateOrdersColumns: ColumnsType<AggregateOrder> = [
     {
-     title: leftAggName,
-     key: 'buyside',
-     dataIndex: 'buyside',
+      title: leftAggName,
+      key: 'buyside',
+      dataIndex: 'buyside',
     },
     {
-     title: rightAggName,
-     key: 'sellside',
-     dataIndex: 'sellside',
+      title: rightAggName,
+      key: 'sellside',
+      dataIndex: 'sellside',
     },
- ];
+  ];
 
-
- const listOfOrdersColumns = [
+  const listOfOrdersColumns = [
     {
-     title: 'Order #',
-     key: 'order_number',
-     dataIndex: 'order_number',
+      title: 'Order #',
+      key: 'order_number',
+      dataIndex: 'order_number',
     },
     {
-     title: 'Order type',
-     key: 'ordertype',
-     dataIndex: 'ordertype',
+      title: 'Order type',
+      key: 'ordertype',
+      dataIndex: 'ordertype',
     },
     {
-     title: 'Price',
-     key: 'price',
-     dataIndex: 'price',
+      title: 'Price',
+      key: 'price',
+      dataIndex: 'price',
     },
     {
-     title: 'Value',
-     key: 'value',
-     dataIndex: 'value',
+      title: 'Value',
+      key: 'value',
+      dataIndex: 'value',
     },
- ];
+  ];
 
   return (
     <div>
-              <Col className="base-content br-t br-b br-l br-r">
-                <Space className="batcher-price" direction="vertical">
-                  <Row>
-                    <Col className="mr-c" span={12}>
-                      <Typography className="batcher-title p-16">
-                        OrderBook
-                      </Typography>
-                    </Col>
-                  </Row>
-                  <Row className="text-center">
-                  <Col span={12} offset={6}>
-                     <Table className="batcher-table ant-typeography center col-offset-6" columns={aggregateOrdersColumns} dataSource={aggregateOrdersForTable} pagination={false} />
-                     </Col>
-                  </Row>
-                  <Space size="large" />
-                  <Row className="text-center">
-                  <Col span={12} offset={6}>
-                  <Table className="batcher-table ant-typeography center" columns={listOfOrdersColumns} dataSource={expandedView ? orderListForTableExpanded : orderListForTable} pagination={false} />
-                     </Col>
-                  </Row>
-                  <Space size="large" />
-                  <Row className="text-center">
-                  <Col span={12} offset={6}>
-                  <Button className="batcher-nav-btn" onClick={() => setView()}><Text underline>{ expandedView ? "See less" : "See more" }</Text></Button>
-                     </Col>
-                  </Row>
-                </Space>
-              </Col>
-      </div>
+      <Col className="base-content br-t br-b br-l br-r">
+        <Space className="batcher-price" direction="vertical">
+          <Row>
+            <Col className="mr-c" span={12}>
+              <Typography className="batcher-title p-16">OrderBook</Typography>
+            </Col>
+          </Row>
+          <Row>
+            <Col lg={3} />
+            <Col lg={18} xs={24}>
+              <Table
+                className="batcher-table ant-typeography center col-offset-6"
+                columns={aggregateOrdersColumns}
+                dataSource={aggregateOrdersForTable}
+                pagination={false}
+              />
+            </Col>
+            <Col lg={3} />
+          </Row>
+          <Space size="large" />
+          <Row className="text-center">
+            <Col lg={3} />
+            <Col lg={18} xs={24}>
+              <Table
+                className="batcher-table ant-typeography center"
+                columns={listOfOrdersColumns}
+                dataSource={expandedView ? orderListForTableExpanded : orderListForTable}
+                pagination={false}
+              />
+            </Col>
+            <Col lg={3} />
+          </Row>
+          <Space size="large" />
+          <Row className="text-center">
+            <Col span={12} offset={6}>
+              <Button className="batcher-nav-btn" onClick={() => setView()}>
+                <Text underline>{expandedView ? 'See less' : 'See more'}</Text>
+              </Button>
+            </Col>
+          </Row>
+        </Space>
+      </Col>
+    </div>
   );
 };
 
