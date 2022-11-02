99cef7b58c241574850e01580983055c938629c8
kienle <kienle371999@gmail.com>
2022-11-02 20:50:37 +0700

    Add the logo to Marigold storage

--- batcher-ui/config/defaultSettings.ts
+++ batcher-ui/config/defaultSettings.ts
@@ -15,7 +15,7 @@ const Settings: LayoutSettings & {
   colorWeak: false,
   title: 'BATCHER',
   pwa: false,
-  logo: 'https://lh3.googleusercontent.com/drive-viewer/AJc5JmQGF2FV1rgeAfnXwcK6MX7y99zqRd5P7Fjr0Xe3hvAoLP1deg_TJPag4PNJ1ZQEe_uMwjHv8Ow=w800-h1722',
+  logo: 'https://storage.cloud.google.com/marigold-stuff/batcher-logo.png?authuser=1',
   iconfontUrl: '',
   disableMobile: true,
 };
