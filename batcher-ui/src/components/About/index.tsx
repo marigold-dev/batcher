c9758750324d3db1248c62d0e7201b4a06c492d7
jason <jason@glottologist.co.uk>
2022-11-10 15:30:10 +0000

    Restructured about text

--- batcher-ui/src/components/About/index.tsx
+++ batcher-ui/src/components/About/index.tsx
@@ -7,13 +7,8 @@ const About: React.FC = () => {
   return (
     <div>
      <Space direction="vertical">
-       <Paragraph strong>Batcher is a batch processing orderbook DEX.  This means that all orders for potential swaps between two pairs of tokens are collected over a finite period (currently 10 minutes) and then the batch is closed to additions. Batcher then waits for the next Oracle price for the token pair and then looks to match the maximum amount of orders at the fairest possible price.
+       <Paragraph strong>Batcher is a batch processing orderbook DEX.  Batcher’s goal is to enable users to deposit tokens with the aim of being swapped at a <Text italic>fair price</Text> with <Text italic>bounded slippage</Text> and almost no <Text italic>impermanent loss</Text>. This means that all orders for potential swaps between two pairs of tokens are collected over a finite period (currently 10 minutes). This is deemed the 'batch'.  After the order collection period is over, the batch is closed to additions. Batcher then waits for the next Oracle price for the token pair.  When this is received, the batch is terminated and then Batcher looks to match the maximum amount of orders at the fairest possible price.
        </Paragraph>
-       <Paragraph strong>
-Batcher’s goal is to enable users to deposit tokens with the aim of being swapped at a <Text italic>fair price</Text> with <Text italic>bounded slippage</Text> and almost no <Text italic>impermanent loss</Text>.
-To do so, users will deposit tokens during a <Text italic>deposit window</Text>; all deposits during this window will be a <Text italic>'batch'</Text>.
-The batch is locked, waiting for an <Text italic>oracle price</Text>. When the later is received, the batch is terminated and the <Text italic>orders</Text> are cleared wherein Batcher will attempt to maximise the number of orders cleared.
-        </Paragraph>
         <Paragraph strong>
 For V1, the <Text italic>deposit window</Text> will be 10 mins and then a wait time of 2 minutes before awaiting the oracle price. Only the <Text italic>tzBTC/USDT</Text> pair will be supported for V1.
         </Paragraph>
