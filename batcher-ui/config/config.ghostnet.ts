1570cd487d4c7aba5ef61c843f1671d5ae9cd6d6
Jason Ridgway-Taylor <jason@glottologist.co.uk>
2023-04-05 14:35:31 +0100

    Updated contract hash on ghostnet

--- batcher-ui/config/config.ghostnet.ts
+++ batcher-ui/config/config.ghostnet.ts
@@ -54,7 +54,7 @@ export default defineConfig({
     REACT_APP_PATH_TO_BATCHER_LOGO: BATCHER_LOGO,
     REACT_APP_TEZOS_NODE_URI: 'https://ghostnet.tezos.marigold.dev',
     REACT_APP_TZKT_URI_API: 'https://api.ghostnet.tzkt.io',
-    REACT_APP_BATCHER_CONTRACT_HASH: 'KT1S7EhW8ZQUzigNTqDrj25kezqEmdNLcxdU',
+    REACT_APP_BATCHER_CONTRACT_HASH: 'KT1PerVYfwhSfKJ3tX6rYooFYhKBpBZx59RY',
   },
   metas: [
     {
