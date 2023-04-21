d0522a81618942b6ee5e2f01b24cf51bbdca893c
Jason Ridgway-Taylor <jason@glottologist.co.uk>
2023-04-21 09:13:22 +0100

    Added new mainnet contract hash

--- batcher-ui/config/config.mainnet.ts
+++ batcher-ui/config/config.mainnet.ts
@@ -54,7 +54,7 @@ export default defineConfig({
     REACT_APP_PATH_TO_BATCHER_LOGO: BATCHER_LOGO,
     REACT_APP_TEZOS_NODE_URI: 'https://mainnet.tezos.marigold.dev',
     REACT_APP_TZKT_URI_API: 'https://api.tzkt.io',
-    REACT_APP_BATCHER_CONTRACT_HASH: 'KT1PTMoNbQJ5CoTJ8htt87XrFneRFGS7etCG',
+    REACT_APP_BATCHER_CONTRACT_HASH: 'KT1CoTu4CXcWoVk69Ukbgwx2iDK7ZA4FMSpJ',
   },
   metas: [
     {
