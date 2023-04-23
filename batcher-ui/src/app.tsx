f66c08de0f88ff32923f5517fbd7e68c8d4fe1e8
Jason Ridgway-Taylor (~misfur-mondut) <57174310+glottologist@users.noreply.github.com>
2023-04-23 22:10:54 +0100

    Ghostnet - Release (#307)
    * Added mainnet admin and oracle address
    
    * Correct mainnet build
    
    * Pointed to mainnet contract
    
    * Corrected UI error code parsing
    
    * pipelines: rework docker build process
    
    * Corrected pair name on mainnet and admin address on both nets
    
    * Added new mainnet contract hash
    
    * pipelines: commit to trigger cicd
    
    * Added michelson files to repo for both mainnet and ghostnet
    
    * Added metadata file
    
    * Fixed issue with wallet disconnecting
    
    * Correctly assigned wallet after reload
    
    * Corrected open and closed holdings
    
    ---------
    
    Co-authored-by: Cyril B <cyril.bourdet.pro@gmail.com>

---
+++ batcher-ui/src/app.tsx
@@ -0,0 +1,47 @@
+import Footer from '@/components/Footer';
+import React, { useState, useEffect } from 'react';
+import RightContent from '@/components/RightContent';
+import { AccountInfo } from '@airgap/beacon-sdk';
+import { BeaconWallet } from '@taquito/beacon-wallet';
+import { PageLoading } from '@ant-design/pro-components';
+import type { RunTimeLayoutConfig } from 'umi';
+import defaultSettings from '../config/defaultSettings';
+import Main from './pages/Main';
+import { Spin, Image } from 'antd';
+import MarigoldLogo from '../img/marigold-logo.png';
+import { TezosToolkit } from '@taquito/taquito';
+import ReactGA from "react-ga4";
+Spin.setDefaultIndicator(<Image src={MarigoldLogo} />);
+
+ReactGA.initialize(GA_TRACKING_ID);
+
+export const initialStateConfig = {
+  loading: <PageLoading />,
+};
+
+export async function getInitialState(): Promise<any> {
+  return {
+    wallet: null,
+    userAddress: null,
+    userAccount: null,
+    settings: defaultSettings,
+  };
+}
+
+export const layout: RunTimeLayoutConfig = ({ initialState, setInitialState }) => {
+  return {
+    rightContentRender: () => <RightContent
+    />,
+    disableContentMargin: false,
+    waterMarkProps: {
+      content: initialState?.currentUser?.name,
+    },
+    footerRender: () => <Footer />,
+    menuHeaderRender: undefined,
+    ...initialState?.settings,
+    childrenRender: () => {
+      return <Main
+      />;
+    },
+  };
+};
