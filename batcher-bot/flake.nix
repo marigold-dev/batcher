0e0498f1db14bcc1fb7c24cf303f893749900b94
Jason Ridgway-Taylor <jason@glottologist.co.uk>
2023-04-28 12:30:34 +0100

    Added skeleton ts project

--- batcher-bot/flake.nix
+++ batcher-bot/flake.nix
@@ -1,26 +1,41 @@
 {
-  description = "Batcher Bot";
-  inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    flake-utils = {
-      url = "github:numtide/flake-utils";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    gitignore = {
-      url = "github:hercules-ci/gitignore.nix";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    dream2nix = {
-      url = "github:nix-community/dream2nix";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  inputs.js2nix = {
+    url = "github:canva-public/js2nix";
+    flake = false;
   };
 
-  outputs = { self, nixpkgs, flake-utils, gitignore, dream2nix, ... }:
-    dream2nix.lib.makeFlakeOutputs {
-      systems = flake-utils.lib.defaultSystems;
-      config.projectRoot = ./.;
-      source = gitignore.lib.gitignoreSource ./.;
+  outputs = { self, nixpkgs, js2nix }:
+    let
+      supportedSystems = [ "x86_64-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin" ];
+      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+      pkgs = forAllSystems (system: nixpkgs.legacyPackages.${system}.extend (self: super: {
+        js2nix = self.callPackage js2nix { };
+      }));
+    in
+    rec {
+      packages = forAllSystems (system: {
+        default =
+          let
+            env = pkgs.${system}.js2nix {
+              package-json = ./package.json;
+              yarn-lock = ./yarn.lock;
+            };
+          in
+          pkgs.${system}.buildEnv {
+            name = "batcher-bot";
+            paths = [
+            ];
+            pathsToLink = [ "/bin" ];
+          };
+      });
 
+      devShells = forAllSystems (system: {
+        default = pkgs.${system}.mkShellNoCC {
+          packages = with pkgs.${system}; [
+            yarn
+          ];
+        };
+      });
     };
 }
