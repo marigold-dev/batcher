55921b2e0a24d7d3bacb8d1fca86f01fe56accf9
Jason Ridgway-Taylor <jason@glottologist.co.uk>
2023-05-01 20:40:45 +0100

    Changed filters on socket messages

--- batcher-bot/src/bot.ts
+++ batcher-bot/src/bot.ts
@@ -42,7 +42,7 @@ const init = async (bot:Telegraf, socketConnection:HubConnection) => {
     socketConnection.on('operations', (msg: any) => {
       if (!msg.data) return;
     for (let i = 0; i < Object.keys(msg.data).length; i++) {
-        console.info("++++ OPERATIONS +++", msg.data[i]);
+        console.info("++++ OPERATIONS +++", msg.data[i].parameter);
       if (msg.data[i].parameter.endpoint == 'deposit') {
         const formattedMessage = format(MessageType.OPERATION, msg.data[i]);
         console.info("formattedMessage", formattedMessage);
