089ccc7aec9b660bf664e6b9f25920481b2e1f00
Jason Ridgway-Taylor <jason@glottologist.co.uk>
2023-05-01 20:48:03 +0100

    Filter for transaction that are contract calls

--- batcher-bot/src/bot.ts
+++ batcher-bot/src/bot.ts
@@ -32,16 +32,21 @@ const init = async (bot:Telegraf, socketConnection:HubConnection) => {
       if (!msg.data) return;
       for (let i = 0; i < Object.keys(msg.data).length; i++) {
         console.info("++++ BIGMAPS +++", msg.data[i]);
+        try{
       if (msg.data[i].path == 'rates_current') {
         const formattedMessage = format(MessageType.BIGMAP, msg.data[i]);
         console.info("formattedMessage", formattedMessage);
         sendToTelegram(bot, formattedMessage[0], formattedMessage[1]);
+      }} catch (error) {
+        console.info("Error parsing bigmap", error.message);
+        console.error(error);
       }
       }
     });
     socketConnection.on('operations', (msg: any) => {
       if (!msg.data) return;
     for (let i = 0; i < Object.keys(msg.data).length; i++) {
+      try{
         console.info("++++ OPERATIONS +++", msg.data[i].parameter);
       if (msg.data[i].parameter) {
       if (msg.data[i].parameter.endpoint == 'deposit') {
@@ -49,6 +54,10 @@ const init = async (bot:Telegraf, socketConnection:HubConnection) => {
         console.info("formattedMessage", formattedMessage);
         sendToTelegram(bot, formattedMessage[0], formattedMessage[1]);
       }
+      }
+      } catch (error) {
+        console.info("Error parsing operation", error.message);
+        console.error(error);
       }
     }
     });
