706dd9f1bb2adde802f3498becf841414417f67d
Jason Ridgway-Taylor <jason@glottologist.co.uk>
2023-05-09 19:05:10 +0100

    Added filter on messages

---
+++ batcher-bot/notifications/src/index.ts
@@ -0,0 +1,30 @@
+import { config } from "dotenv";
+import { TezosToolkit } from "@taquito/taquito";
+import { Telegraf } from "telegraf";
+import { interval } from "rxjs";
+import { start } from "./bot";
+import { HubConnection, HubConnectionBuilder } from "@microsoft/signalr";
+import {
+  NotificationsEnabled,
+  TelegramMessageContents,
+  MessageType,
+} from "./types";
+
+config();
+
+const botToken = process.env["BOT_TOKEN"];
+const tzktUri = process.env["TZKT_URI_API"];
+const notifications = process.env["NOTIFICATIONS"];
+
+const bot = new Telegraf(botToken);
+const socketConnection = new HubConnectionBuilder()
+  .withUrl(tzktUri + "/v1/ws")
+  .build();
+
+let notifications_enabled: NotificationsEnabled = {
+  deposits: notifications.includes("deposits"),
+  rates: notifications.includes("rates"),
+  batch_status: notifications.includes("batch_status"),
+};
+
+start(bot, socketConnection, notifications_enabled);
