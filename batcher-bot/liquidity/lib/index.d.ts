81a18627d5a8bb81a25d1fd6e1c455fffe3a2456
Arthur Guillon <arthur@marigold.dev>
2023-05-16 15:55:37 +0200

    Add a dependency to Taquito

--- batcher-bot/liquidity/lib/index.d.ts
+++ batcher-bot/liquidity/lib/index.d.ts
@@ -1,7 +1,2 @@
 #!/usr/bin/env node
-declare const chalk: any;
-declare const clear: any;
-declare const figlet: any;
-declare const path: any;
-declare const program: any;
-declare const options: any;
+export {};
