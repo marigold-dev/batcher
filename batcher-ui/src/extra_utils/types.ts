f3e101e6c37852f1ce0d54039042edacdb71a8b3
Jason Ridgway-Taylor (~misfur-mondut) <57174310+glottologist@users.noreply.github.com>
2023-05-24 17:10:36 +0100

    Disabled redeem button if there are no cleared holdings (#341)

--- batcher-ui/src/extra_utils/types.ts
+++ batcher-ui/src/extra_utils/types.ts
@@ -238,6 +238,7 @@ export type HoldingsProps = {
   setClearedHoldings: Dispatch<SetStateAction<Map<string, number>>>;
   updateAll: boolean;
   setUpdateAll: Dispatch<SetStateAction<boolean>>;
+  hasClearedHoldings: boolean;
 };
 
 export type Volumes = {
@@ -265,3 +266,4 @@ export type tokens = {
   buy_token_name: string;
   sell_token_name: string;
 };
+
