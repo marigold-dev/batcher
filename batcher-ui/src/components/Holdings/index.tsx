d1f67d4ef3c0e55924f624841267ee18d0cd2ee1
Jason Ridgway-Taylor (~misfur-mondut) <57174310+glottologist@users.noreply.github.com>
2023-07-17 11:48:08 +0300

    Cancellations (#375)
    * Added refunds of tokens and fess on cancelation
    
    * Added check that batch is open in order to cancel
    
    * Built contract
    
    * Added UI changes for cancellations
    
    * Added refund of tokens back in, along with burning fee

--- batcher-ui/src/components/Holdings/index.tsx
+++ batcher-ui/src/components/Holdings/index.tsx
@@ -3,19 +3,23 @@ import { Button, Space, Typography, Col, message, Table } from 'antd';
 import '@/components/Exchange/index.less';
 import '@/components/Holdings/index.less';
 import '@/global.less';
-import { HoldingsProps } from '@/extra_utils/types';
+import { HoldingsProps, holding } from '@/extra_utils/types';
 import { useModel } from 'umi';
 import { zeroHoldings } from '@/extra_utils/utils';
 const Holdings: React.FC<HoldingsProps> = ({
   tezos,
   contractAddress,
   openHoldings,
+  closedHoldings,
   clearedHoldings,
   setOpenHoldings,
   setClearedHoldings,
   updateAll,
   setUpdateAll,
   hasClearedHoldings,
+  hasClosedHoldings,
+  hasOpenHoldings,
+  tokenMap,
 }: HoldingsProps) => {
   const { initialState } = useModel('@@initialState');
 
@@ -26,27 +30,135 @@ const Holdings: React.FC<HoldingsProps> = ({
     }, 5000);
   };
 
-  const generateHoldings = (dict: Map<string, number>) => {
+  const generateHoldings = (dict: Map<string, holding>) => {
     var data = [];
+
     for (const key of dict) {
+      let buyHolding = key[1].buy_token_holding;
+      let sellHolding = key[1].sell_token_holding;
+      if(buyHolding > 0 || sellHolding > 0 ){
       data.push({
-        token: key[0],
-        holding: key[1],
+        pair: key[0],
+        buySideHolding: buyHolding,
+        sellSideHolding: sellHolding,
       });
+      };
     }
+
+    return data;
+  };
+
+  const listOfColumnsWithCancel = [
+    {
+      title: 'Pair',
+      key: 'pair',
+      dataIndex: 'pair',
+    },
+    {
+      title: 'Buy Side Holding',
+      key: 'buySideHolding',
+      dataIndex: 'buySideHolding',
+    },
+    {
+      title: 'Sell Side Holding',
+      key: 'sellSideHolding',
+      dataIndex: 'sellSideHolding',
+    },
+    {
+      title: 'Cancel Orders?',
+      key: 'cancel',
+      dataIndex: 'cancel',
+    },
+  ];
+  const listOfColumns = [
+    {
+      title: 'Pair',
+      key: 'pair',
+      dataIndex: 'pair',
+    },
+    {
+      title: 'Buy Side Holding',
+      key: 'buySideHolding',
+      dataIndex: 'buySideHolding',
+    },
+    {
+      title: 'Sell Side Holding',
+      key: 'sellSideHolding',
+      dataIndex: 'sellSideHolding',
+    },
+  ];
+
+  const generateCancelButton = (pair: string) => {
     return (
       <>
-        {data.map((h) => (
-          <React.Fragment key={h.token}>
-            <Typography>
-              {' '}
-              {h.holding} {h.token} |{' '}
-            </Typography>
+          <React.Fragment key={pair}>
+            <Button className="btn-content mtb-10" type="primary" onClick={() => cancelHoldings(pair)} danger>Cancel</Button>
           </React.Fragment>
-        ))}
       </>
     );
   };
+
+  const generateOpenHoldings = (dict: Map<string, holding>) => {
+    var data = [];
+
+    for (const key of dict) {
+      let buyHolding = key[1].buy_token_holding;
+      let sellHolding = key[1].sell_token_holding;
+      if(buyHolding > 0 || sellHolding > 0 ){
+      data.push({
+        pair: key[0],
+        buySideHolding: buyHolding,
+        sellSideHolding: sellHolding,
+        cancel: generateCancelButton(key[0])
+      });
+      };
+    }
+
+    return data;
+  };
+
+
+  const cancelHoldings = async (pair: string): Promise<void> => {
+
+    let loading = function () {
+      return undefined;
+    };
+
+    try {
+      tezos.setWalletProvider(initialState.wallet);
+      const contractWallet = await tezos.wallet.at(contractAddress);
+      let methods = contractWallet.parameterSchema.ExtractSignatures();
+      console.info("Methods", methods);
+
+      const holding = openHoldings.get(pair);
+      console.info("Holding", holding);
+
+      let cancel_op = await contractWallet.methodsObject.cancel([holding.buy_token_name, holding.sell_token_name]).send();
+
+
+      if (cancel_op) {
+        loading = message.loading('Attempting to cancel open holdings...', 0);
+        const confirm = await cancel_op.confirmation();
+        if (!confirm.completed) {
+          message.error('Failed to cancel open holdings');
+          console.error('Failed to cancel open holdings' + confirm);
+        } else {
+          setOpenHoldings(new Map<string, holding>());
+          loading();
+          message.success('Successfully cancelled open holdings');
+          triggerUpdate();
+        }
+      } else {
+        throw new Error('Failed to cancel open holdings');
+      }
+    } catch (error: any) {
+      loading();
+      message.error('Unable to redeem holdings : ' + error.message);
+      console.error('Unable to redeem holdings' + error);
+  };
+  };
+
+
   const redeemHoldings = async (): Promise<void> => {
     let loading = function () {
       return undefined;
@@ -83,32 +195,76 @@ const Holdings: React.FC<HoldingsProps> = ({
   return (
     <Col className="base-content br-t br-b br-l br-r">
       <Space className="batcher-price" direction="vertical">
-        <Typography className="batcher-title p-16">Open/Closed Batches</Typography>
+        <Typography className="batcher-title p-16">Open Batches (Cancellable)</Typography>
+        { hasOpenHoldings ?
+        (
         <Col className="batcher-holding-content br-t br-b br-l br-r pd-25 tx-align" span={24}>
           <Space direction="horizontal">
-            <Typography>Holdings =&gt; </Typography>| {generateHoldings(openHoldings)}
+              <Table
+                className="batcher-table ant-typeography center"
+                columns={listOfColumnsWithCancel}
+                rowKey="pair"
+                dataSource={generateOpenHoldings(openHoldings)}
+                pagination={false}
+              />
           </Space>
         </Col>
+        ) : (
+        <Col className="batcher-holding-content br-t br-b br-l br-r pd-25 tx-align" span={24}>
+            <Typography> No closed holdings </Typography>
+          </Col>
+        )}
+      </Space>
+      <Space className="batcher-price" direction="vertical">
+        <Typography className="batcher-title p-16">Closed Batches</Typography>
+        { hasClosedHoldings ?
+        (<Col className="batcher-holding-content br-t br-b br-l br-r pd-25 tx-align" span={24}>
+          <Space direction="horizontal">
+              <Table
+                className="batcher-table ant-typeography center"
+                columns={listOfColumns}
+                rowKey="pair"
+                dataSource={generateHoldings(closedHoldings)}
+                pagination={false}
+              />
+          </Space>
+        </Col> ) :
+
+        (
+        <Col className="batcher-holding-content br-t br-b br-l br-r pd-25 tx-align" span={24}>
+            <Typography> No closed holdings </Typography>
+          </Col>
+          )
+        }
       </Space>
       <Space className="batcher-price" direction="vertical">
         <Typography className="batcher-title p-16">Cleared Batches (Redeemable)</Typography>
-        <Col className="batcher-holding-content br-t br-b br-l br-r pd-25 tx-align" span={24}>
+          { hasClearedHoldings ?
+          ( <Col className="batcher-holding-content br-t br-b br-l br-r pd-25 tx-align" span={24}>
           <Space direction="horizontal">
-            <Typography>Holdings =&gt; </Typography>
-            {generateHoldings(clearedHoldings)}
+              <Table
+                className="batcher-table ant-typeography center"
+                columns={listOfColumns}
+                rowKey="pair"
+                dataSource={generateHoldings(clearedHoldings)}
+                pagination={false}
+              />
           </Space>
-        </Col>
-      </Space>
       <Space className="batcher-price" direction="vertical">
         <Col className="batcher-redeem-btn">
-          {hasClearedHoldings ? (
             <Button className="btn-content mtb-25" type="primary" onClick={redeemHoldings} danger>
               Redeem
             </Button>
-          ) : (
-            <div></div>
-          )}
         </Col>
+      </Space>
+             </Col>
+ ) :
+          (
+          <Col className="batcher-holding-content br-t br-b br-l br-r pd-25 tx-align" span={24}>
+            <Typography> No cleared holdings </Typography>
+          </Col>
+      )
+      }
       </Space>
     </Col>
   );
