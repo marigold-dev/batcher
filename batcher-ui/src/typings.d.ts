0d5364416d7f59d5c2e774953c0374d3799ccc92
Cyril B <cyril.bourdet.pro@gmail.com>
2023-07-25 12:15:23 +0200

    add google analytics tracking (#385)

--- batcher-ui/src/typings.d.ts
+++ batcher-ui/src/typings.d.ts
@@ -31,3 +31,4 @@ declare const REACT_APP_TZBTC_HASH;
 declare const REACT_APP_USDT_HASH;
 declare const REACT_APP_BATCHER_URI;
 declare const REACT_APP_PATH_TO_BATCHER_LOGO;
+declare const GA_TRACKING_ID;
