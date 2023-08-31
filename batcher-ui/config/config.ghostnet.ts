c31da2e333fa144d1658a94827ea604a3a54f463
Jason Ridgway-Taylor (~misfur-mondut) <57174310+glottologist@users.noreply.github.com>
2023-08-31 16:46:59 +0300

    Enforce side (#392)
    * Added enforcement of correct side when depositing tokens
    
    * Built contract
    
    * :/:Dockerfile.tick.ghostnet
    
    * Added correct tokens for ghostnet contract
    
    * Switched UI to new ghostnet contract

--- batcher-ui/config/config.ghostnet.ts
+++ batcher-ui/config/config.ghostnet.ts
@@ -54,7 +54,7 @@ export default defineConfig({
     REACT_APP_PATH_TO_BATCHER_LOGO: BATCHER_LOGO,
     REACT_APP_TEZOS_NODE_URI: 'https://ghostnet.tezos.marigold.dev',
     REACT_APP_TZKT_URI_API: 'https://api.ghostnet.tzkt.io',
-    REACT_APP_BATCHER_CONTRACT_HASH: 'KT1Txkbx4sDc3qXQubG48vL3ojoTK4HH8Ci8',
+    REACT_APP_BATCHER_CONTRACT_HASH: 'KT1UPMR3WkoFRJYmBBbvv4Z9bBhKYuhCx7Cq',
   },
   metas: [
     {
