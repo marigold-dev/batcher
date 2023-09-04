a27c85698da848c7eeee1754ba4105fa5123d169
Jason Ridgway-Taylor <jason@glottologist.co.uk>
2023-09-04 13:03:43 +0300

    :/:batcher-ui/config/config.ghostnet.ts

--- batcher-ui/config/config.ghostnet.ts
+++ batcher-ui/config/config.ghostnet.ts
@@ -54,7 +54,7 @@ export default defineConfig({
     REACT_APP_PATH_TO_BATCHER_LOGO: BATCHER_LOGO,
     REACT_APP_TEZOS_NODE_URI: 'https://ghostnet.tezos.marigold.dev',
     REACT_APP_TZKT_URI_API: 'https://api.ghostnet.tzkt.io',
-    REACT_APP_BATCHER_CONTRACT_HASH: 'KT1UPMR3WkoFRJYmBBbvv4Z9bBhKYuhCx7Cq',
+    REACT_APP_BATCHER_CONTRACT_HASH: 'KT1Gpu3ahjc9pyS4VrwMxsQ7KJWqrcqPoisR',
     GA_TRACKING_ID: 'G-2K59PEELC8',
   },
   metas: [
