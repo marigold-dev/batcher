bf17a14f1426fc77ee7e6db3ae2b6c95b65c6b0c
Quentin Burg <quentin@marigold.dev>
2023-09-07 10:16:12 +0200

    :mag: write open graph metadata

--- batcher-ui/pages/_app.tsx
+++ batcher-ui/pages/_app.tsx
@@ -17,12 +17,37 @@ config.autoAddCss = false;
 process.env.NEXT_PUBLIC_GA_TRACKING_ID &&
   ReactGA.initialize(process.env.NEXT_PUBLIC_GA_TRACKING_ID);
 
+
 const App = ({ Component }: AppProps) => {
   return (
     <div>
       <Head>
         <link rel="shortcut icon" href="/favicon.ico" />
         <title>BATCHER</title>
+        <meta property="og:locale" content="en_US" />
+        <meta property="og:title" content="Batcher DEX" />
+        <meta
+          property="og:description"
+          content="The aim of the batch clearing dex is to enable users to deposit tokens with the aim of being swapped at a fair price with bounded slippage and almost no impermanent loss.."
+        />
+        <meta property="og:url" content={process.env.NEXT_PUBLIC_BATCHER_URI} />
+        <meta property="og:site_name" content="Batcher DEX" />
+        <meta
+          property="og:image"
+          content={process.env.NEXT_PUBLIC_PATH_TO_BATCHER_LOGO}
+        />
+        <meta property="twitter:card" content="summary" />
+        <meta
+          property="twitter:description"
+          content="The aim of the batch clearing dex is to enable users to deposit tokens with the aim of being swapped at a fair price with bounded slippage and almost no impermanent loss.."
+        />
+        <meta property="twitter:title" content="Batcher DEX" />
+        <meta property="twitter:site" content="@Marigold_Dev" />
+        <meta property="twitter:creator" content="@Marigold_Dev" />
+        <meta
+          property="twitter:image"
+          content={process.env.NEXT_PUBLIC_PATH_TO_BATCHER_LOGO}
+        />
       </Head>
       <Provider store={store}>
         <TezosToolkitProvider>
