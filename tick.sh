7e60c32d3e1db0dd1df3e522f704a7361b23809a
Jason Ridgway-Taylor <jason@glottologist.co.uk>
2023-11-08 16:03:56 +0530

    Adjusted tick script to tick more pairs and also tick the market maker

--- tick.sh
+++ tick.sh
@@ -6,6 +6,7 @@ while getopts b:f: flag
 do
   case "${flag}" in
     b) batcher_address=${OPTARG};;
+    m) market_maker_address=${OPTARG};;
     f) frequency=${OPTARG};;
   esac
 done
@@ -13,7 +14,7 @@ done
 FREQ=$(($frequency))
 
 # declare -a TICKERS=("tzBTC-USDT" "EURL-tzBTC")
-declare -a TICKERS=("tzBTC/USDT" "tzBTC/EURL")
+declare -a TICKERS=("BTCtz/USDT" "BTCtz/USDtz" "tzBTC/EURL" "tzBTC/USDT" "tzBTC/USDtz")
 
 tick_ticker(){
 
@@ -28,6 +29,19 @@ tick_ticker(){
   set -e
 }
 
+tick_mm(){
+
+  set +e
+  echo "Tick market maker contract - $market_maker_address"
+
+  octez-client transfer 0 from oracle_account to $market_maker_address \
+    --entrypoint tick \
+    --arg "\"Unit\"" \
+    --burn-cap 2
+
+  set -e
+}
+
 post_op (){
 
 for i in "${TICKERS[@]}"
@@ -36,6 +50,8 @@ do
    tick_ticker "$i"
   sleep 5
 done
+
+tick_mm
 
 }
 
